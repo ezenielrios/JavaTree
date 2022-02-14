6aef41d782f44956b15589f42181bdb2aca1107f
Ezeniel Rios <75400864+ezenielrios@users.noreply.github.com>
2022-02-14 15:48:15 -0500

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args) {
+ //I'm going to introduce myself and print a tree to the screen
+ System.out.println("Hey there, I'm Ezeniel and I'm Learning to code in Java!");
+  System.out.println("I'm going to plant a tree today!");
+  System.out.println("  *  ");
+  System.out.println(" *** ");
+  System.out.println(" *** ");
+  System.out.println("  *  ");
+  System.out.println("  *  ");    
+  }
+}
